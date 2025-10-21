6640e7ade5ac2193b309c9279cd221890cdcb2f3
micaelaalvarezj <micaela.alvarezj@gmail.com>
2025-10-21 21:24:24 +0200

    formulario A primera parte

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,46 @@
 import React from "react";
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { Link } from "react-router-dom";
+import Form from "../components/Form";
 
 const HomePage = () => {
 
+    const [ name, setName ] = useState("");
+    const [ slogan, setSlogan ] = useState("");
+    const [ repo, setRepo ] = useState("");
+    const [ demo, setDemo ] = useState("");
+    const [ technologies, setTechnologies ] = useState("");
+    const [ desc, setDesc ] = useState("");
+
+    const updateName = (value) => {
+        setName (value)}
+    const updateSlogan = (value) => {
+        setSlogan (value)}
+    const updateRepo = (value) => {
+        setRepo (value)}
+    const updateTechnologies = (value) => {
+        setTechnologies (value)}
+    const updateDemo = (value) => {
+        setDemo (value)}
+    const updateDesc = (value) => {
+        setDesc (value)}
+
     return (
          <>
-            <h1>Home</h1>
+            <Form 
+                updateName={updateName}
+                name={name}
+                updateSlogan={updateSlogan}
+                slogan={slogan}
+                updateRepo={updateRepo}
+                repo={repo}
+                updateTechnologies={updateTechnologies}
+                technologies={technologies}
+                updateDemo={updateDemo}
+                demo={demo}
+                updateDesc={updateDesc}
+                desc={desc}
+            />
         </>
     )
 }
