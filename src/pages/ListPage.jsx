dfdbbf201d3db5c0b41033c03cc8ef7eda07d10a
Rosana-Moya <rosanamoya88@gmail.com>
2025-10-25 13:53:54 +0200

    comprobando

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,10 +1,12 @@
 import "react";
+import HeaderList from "../components/HeaderList";
+
 
 const ListPage = () => {
 
     return (
         <>
-        <h1>Hola</h1>
+        <HeaderList/>
         </>
     )
 } 
