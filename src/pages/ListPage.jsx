46e0da31287444e9f66759d6fee391384e6ca82b
Michelle-1982 <michellepachecopantoja@gmail.com>
2025-10-25 13:15:28 +0200

    listpage

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,11 +1,53 @@
 import "react";
 
-const ListPage = () => {
 
-    return (
+const ListPage = () => {
+const projects =[
+    {
+        name: "Proyecto 1",
+        slogan: "Slogan del proyecto 1",
+        repo: "Repositorio del proyecto 1",
+        demo : "Demo del proyecto 1",
+        technologies: "Tecnologías del proyecto 1",
+        desc: "Descripción del proyecto 1",
+        autor: "Autor del proyecto 1",
+        job: "Puesto del autor 1",
+    },
+    {
+        name: "Proyecto 2",
+        slogan: "Slogan del proyecto 2",
+        repo: "Repositorio del proyecto 2",
+        demo : "Demo del proyecto 2",
+        technologies: "Tecnologías del proyecto 2",
+        desc: "Descripción del proyecto 2",
+        autor: "Autor del proyecto 2",
+        job: "Puesto del autor 2",
+    },
+];  
+     return (
         <>
-        <h1>Hola</h1>
+        <ul> {projects.map (project => {
+            return (
+               <Card
+            
+        name={project.name}
+        slogan={project.slogan}
+        repo={project.repo}
+        technologies={project.technologies}
+        demo={project.demo}
+        desc={project.desc}
+        autor={project.autor}
+        job={project.job}
+        
+               
+               />
+            )
+        })}</ul> 
         </>
-    )
-} 
+    
+    );
+
+};
+
+
 export default ListPage;
