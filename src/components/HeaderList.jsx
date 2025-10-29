74941c01e48c66a0cd5a62daa0cee5cb23ed5fe5
Rosana-Moya <rosanamoya88@gmail.com>
2025-10-29 16:01:01 +0100

    boton headerlist arreglado

--- src/components/HeaderList.jsx
+++ src/components/HeaderList.jsx
@@ -1,39 +1,49 @@
-import "react"
-import "../styles/headerList.css"
+import React from "react";
+import "../styles/headerList.css";
 
 const HeaderList = () => {
-    return (
-        <>
-        <header className="header-list">
-            <div className="grid-contain">
-                <div className="row-Header">
-                    <div className="icon">
-                        <img src="/laptop-code-solid.svg" alt="Vector decorativo" className="header-icon" />
-                        <h2 className="header-subtitle">Proyectos Molones</h2>
-                    </div>
-                    <img src="/adalab.png" alt="Logo Adalab" className="header-logo" />
-                </div>
-                <div className="row">   
-                    <h1 className=".header-list__title Text-style-3">
-                        Proyectos Molones
-                    </h1>
-                </div>
-                <div className="row-2">
-                    <p className="header-list__subtitle">
-                        Escaparate en línea para recoger ideas a través de la tecnología.
-                    </p>
-                </div>
-                <div className="row-3">
-                    <div className="Rectangle">
-                        <span className="header-list">
-                            NUEVO PROYECTO
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </header>
-    </>
-    )
-}
+  return (
+    <header className="header-list">
+      <div className="grid-contain">
 
-export default HeaderList
+        <div className="row-Header">
+          <div className="icon">
+            <img
+              src="/laptop-code-solid.svg"
+              alt="Vector decorativo"
+              className="header-icon"
+            />
+            <h2 className="header-subtitle">Proyectos Molones</h2>
+          </div>
+
+          <img
+            src="/adalab.png"
+            alt="Logo Adalab"
+            className="header-logo"
+          />
+        </div>
+
+        <div className="row">
+          <h1 className="header-list__title Text-style-3">
+            Proyectos Molones
+          </h1>
+        </div>
+
+        <div className="row-2">
+          <p className="header-list__subtitle">
+            Escaparate en línea para recoger ideas a través de la tecnología.
+          </p>
+        </div>
+
+        <div className="row-3">
+          <button className="button-new-project">
+            NUEVO PROYECTO
+          </button>
+        </div>
+
+      </div>
+    </header>
+  );
+};
+
+export default HeaderList;
